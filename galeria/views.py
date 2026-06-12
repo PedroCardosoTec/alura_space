73a774bb56e5b78720257048388aa65af35c03ff
Pedro Cardoso <pedro.cardoso@exata.it>
2026-06-12 13:32:42 -0300

    fix: adcionando caminho dinamico templates

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return render(request,'/home/pedro_cardoso/alura-space/templates/galeria/index.html')
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
-    return render(request, '/home/pedro_cardoso/alura-space/templates/galeria/imagem.html')
+    return render(request, 'galeria/imagem.html')
